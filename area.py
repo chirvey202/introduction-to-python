7f2a39569d250e9cdef3819ff20ca51753166773
chirvey202 <98695523+chirvey202@users.noreply.github.com>
2023-01-24 15:46:01 +0300

    Add files via upload

---
+++ area.py
@@ -0,0 +1,5 @@
+"""a program to input the radius and calculate the area of a circle"""
+pie=3.142
+r=int(input("Enter the radius of the circle:"))
+area=pie*r*r
+print("area of the circle is:",area)
